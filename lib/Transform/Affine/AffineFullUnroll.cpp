e157b47b4dab2891acf0650f8fd80261d4c9d6e6
shrikardongre <shrikardongre@gmail.com>
2024-12-27 23:54:54 +0530

    day 3

---
+++ lib/Transform/Affine/AffineFullUnroll.cpp
@@ -0,0 +1,23 @@
+#include "lib/Transform/Affine/AffineFullUnroll.h"
+#include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/Affine/LoopUtils.h"
+#include "mlir/include/mlir/Pass/Pass.h"
+
+namespace mlir {
+namespace tutorial {
+
+using mlir::affine::AffineForOp;
+using mlir::affine::loopUnrollFull;
+
+void AffineFullUnrollPass::runOnOperation() {
+  
+  getOperation().walk([&](AffineForOp op) {
+    if (failed(loopUnrollFull(op))) {
+      op.emitError("unrolling failed");
+      signalPassFailure();
+    }
+  });
+}
+
+} // namespace tutorial
+} // namespace mlir
