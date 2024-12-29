90f66b7820a9561eeac077c01a4036368ede5303
shrikardongre <shrikardongre@gmail.com>
2024-12-29 13:45:08 +0530

    migrating to tablegen

--- lib/Transform/Affine/AffineFullUnroll.cpp
+++ lib/Transform/Affine/AffineFullUnroll.cpp
@@ -1,48 +1,32 @@
 #include "lib/Transform/Affine/AffineFullUnroll.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
-#include "mlir/IR/PatternMatch.h"
-#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
 #include "mlir/Dialect/Affine/LoopUtils.h"
 #include "mlir/include/mlir/Pass/Pass.h"
 
 namespace mlir {
 namespace tutorial {
 
+#define GEN_PASS_DEF_AFFINEFULLUNROLL
+#include "lib/Transform/Affine/Passes.h.inc"
+
 using mlir::affine::AffineForOp;
 using mlir::affine::loopUnrollFull;
 
-void AffineFullUnrollPass::runOnOperation() {
-  
-  getOperation().walk([&](AffineForOp op) {
-    if (failed(loopUnrollFull(op))) {
-      op.emitError("unrolling failed");
-      signalPassFailure();
-    }
-  });
-}
-// A rewrite pattern
-struct AffineFullUnrollPattern :
-  public OpRewritePattern<AffineForOp> {
-  AffineFullUnrollPattern(mlir::MLIRContext *context)
-      : OpRewritePattern<AffineForOp>(context, /*benefit=*/1) {}
+struct AffineFullUnroll : impl::AffineFullUnrollBase<AffineFullUnroll> {
+  using AffineFullUnrollBase::AffineFullUnrollBase;
 
-  LogicalResult matchAndRewrite(AffineForOp op,
-                                PatternRewriter &rewriter) const override {
-    // This is technically not allowed, since in a RewritePattern all
-    // modifications to the IR are supposed to go through the `rewriter` arg,
-    // but it works for our limited test cases.
-    return loopUnrollFull(op);
+  void runOnOperation() {
+    getOperation()->walk([&](AffineForOp op) {
+      if (failed(loopUnrollFull(op))) {
+        op.emitError("unrolling failed");
+        signalPassFailure();
+      }
+    });
   }
 };
 
-// A pass that invokes the pattern rewrite engine.
-void AffineFullUnrollPassAsPatternRewrite::runOnOperation() {
-  mlir::RewritePatternSet patterns(&getContext());
-  patterns.add<AffineFullUnrollPattern>(&getContext());
-  // One could use GreedyRewriteConfig here to slightly tweak the behavior of
-  // the pattern application.
-  (void)applyPatternsAndFoldGreedily(getOperation(), std::move(patterns));
-}
+
 
 } // namespace tutorial
 } // namespace mlir
