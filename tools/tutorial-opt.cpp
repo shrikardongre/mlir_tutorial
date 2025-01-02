9182a7bfc18688958334a2b0ca4671783b31c21a
shrikardongre <shrikardongre@gmail.com>
2025-01-02 17:20:33 +0530

    adding traits

--- tools/tutorial-opt.cpp
+++ tools/tutorial-opt.cpp
@@ -2,6 +2,7 @@
 #include "lib/Dialect/Poly/PolyDialect.h"
 #include "lib/Transform/Arith/MulToAdd.h"
 #include "mlir/include/mlir/InitAllDialects.h"
+#include "mlir/include/mlir/InitAllPasses.h"
 #include "mlir/include/mlir/Pass/PassManager.h"
 #include "mlir/include/mlir/Pass/PassRegistry.h"
 #include "mlir/include/mlir/Tools/mlir-opt/MlirOptMain.h"
@@ -10,6 +11,7 @@ int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   registry.insert<mlir::tutorial::poly::PolyDialect>();
   mlir::registerAllDialects(registry);
+  mlir::registerAllPasses();
 
   mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPass>();
   mlir::PassRegistration<mlir::tutorial::AffineFullUnrollPassAsPatternRewrite>();
